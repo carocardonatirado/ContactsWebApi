237a26a40d5ff1381033569cafb2a4b60b68fde6
carocardonatirado <carocardonatirado@gmail.com>
2019-08-20 16:16:20 -0500

    Initial check-in of module ContactsWebApi

---
+++ Contacts/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Contacts.Models
+{
+    public class Contact
+    {
+        public Contact()
+        {
+        }
+    }
+}
---
+++ Contacts/Models/ContactsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Models
+{
+    public class ContactsContext : DbContext
+    {
+        public ContactsContext(DbContextOptions<ContactsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
